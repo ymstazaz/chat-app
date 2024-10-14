74ff7d379651fe0d8bfbe449930177b20f7c6ba1
ymstazaz <r37a45d@gmail.com>
2024-10-15 00:09:15 +0900

    新規ルーム作成画面！エラー！
    ユーザーIDが引っ張れてないせいでdbへの新規保存もできていない！

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,21 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
-end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  #  create
+    # binding.pryフォームから送信されるパラムスを確認するためにビンディングプレイを記述（ローカルで開いたものをターミナルで確認テストするためのコード）
+
+    private
+#  ストロングパラメータを設定。（余計な物を保存しないように設定）
+    def room_params
+      params.require(:room).permit(:name, user_ids: [])
+    end
+ end
