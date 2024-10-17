10f18ae5ee9e9de2f40185f5df52c966090a4158
ymstazaz <r37a45d@gmail.com>
2024-10-17 10:13:16 +0900

    エラー修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,11 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    @message.save
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
